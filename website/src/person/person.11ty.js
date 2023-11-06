79f7cd3a3a0433aa58277cc506a9c13ca1ccbe5b
Lumi Xu <102558017+Lumi-works@users.noreply.github.com>
2023-11-06 09:52:40 -0600

    add choi state

--- website/src/person/person.11ty.js
+++ website/src/person/person.11ty.js
@@ -1,150 +1,159 @@
 const data = async () => {
+  const zooflm = await import("@phfaist/zoodb/zooflm");
 
-    const zooflm = await import('@phfaist/zoodb/zooflm');
-
-    return {
-        pagination: {
-            data: 'zoodb.objects.person',
-            size: 1,
-            resolve: 'values',
-            addAllPagesToCollections: true,
-            alias: 'person',
-        },
-        date: '2000-01-01', // should be ignored
-        tags: ['allPages', 'person'],
-        eleventyComputed: {
-            permalink: (data) =>
-                data.zoodb.zoo_object_permalink('person', data.person.person_id) + '.html',
-            title: (data) => zooflm.render_text_standalone(data.person.name),
-            person_name: (data) => zooflm.render_text_standalone(data.person.name),
-            date: (data) => {
-                // injection hack to get correct page date property!
-                // https://github.com/11ty/eleventy/issues/2199#issuecomment-1027362151
-                data.page.date = new Date(data.person._zoodb.git_last_modified_date);
-                return data.page.date;
-            },
-        },
-    };
+  return {
+    pagination: {
+      data: "zoodb.objects.person",
+      size: 1,
+      resolve: "values",
+      addAllPagesToCollections: true,
+      alias: "person",
+    },
+    date: "2000-01-01", // should be ignored
+    tags: ["allPages", "person"],
+    eleventyComputed: {
+      permalink: (data) =>
+        data.zoodb.zoo_object_permalink("person", data.person.person_id) +
+        ".html",
+      title: (data) => zooflm.render_text_standalone(data.person.name),
+      person_name: (data) => zooflm.render_text_standalone(data.person.name),
+      date: (data) => {
+        // injection hack to get correct page date property!
+        // https://github.com/11ty/eleventy/issues/2199#issuecomment-1027362151
+        data.page.date = new Date(data.person._zoodb.git_last_modified_date);
+        return data.page.date;
+      },
+    },
+  };
 };
 
 const render = async (data) => {
+  const { person, zoodb } = data;
 
-    const { person, zoodb } = data;
+  const zoo_flm_environment = zoodb.zoo_flm_environment;
 
-    const zoo_flm_environment = zoodb.zoo_flm_environment;
+  const zooflm = await import("@phfaist/zoodb/zooflm");
+  const { sqzhtml } = await import("@phfaist/zoodb/util/sqzhtml");
 
-    const zooflm = await import('@phfaist/zoodb/zooflm');
-    const { sqzhtml } = await import('@phfaist/zoodb/util/sqzhtml');
+  const render_doc_fn = (render_context) => {
+    const { ne, rdr, rdrblock, ref } = zooflm.make_render_shorthands({
+      render_context,
+    });
 
-    const render_doc_fn = (render_context) => {
+    let s = "";
 
-        const { ne, rdr, rdrblock, ref } = zooflm.make_render_shorthands({ render_context });
-
-        let s = '';
-
-        s += sqzhtml`
+    s += sqzhtml`
 <article>
-<h1>${ rdr(person.name) }</h1>
+<h1>${rdr(person.name)}</h1>
 
 <h2>Biography</h2>
-<div style="margin: 1.5rem 0px">${ rdrblock(person.biography) }</div>`;
+<div style="margin: 1.5rem 0px">${rdrblock(person.biography)}</div>;
 
-        // add Kraus Operators
-        if (person.kraus_operators != null) {
-            s += sqzhtml`
+
+<h2>Representation</h2>`;
+
+    // add Kraus Operators
+    if (person.kraus_operators != null) {
+      s += sqzhtml`
         <h3>Kraus Operators</h3>
-        <div>${ rdrblock(person.kraus_operators) }</div>`;
-        }
+        <div>${rdrblock(person.kraus_operators)}</div>`;
+    }
 
-        // add isometry
-        if (person.isometry != null) {
-            s += sqzhtml`
+    // add isometry
+    if (person.isometry != null) {
+      s += sqzhtml`
         <h3>Isometry</h3>
-        <div>${ rdrblock(person.isometry) }</div>`;
-        }
+        <div>${rdrblock(person.isometry)}</div>`;
+    }
 
+    // add chio state
+    if (person.choi_state != null) {
+      s += sqzhtml`
+          <h3>Choi State</h3>
+          <div>${rdrblock(person.choi_state)}</div>`;
+    }
 
-        const relations = person.relations ?? {};
+    const relations = person.relations ?? {};
 
-        if (relations.spouse != null) {
-            s += sqzhtml`
+    if (relations.spouse != null) {
+      s += sqzhtml`
 
 
 
 <h2>Spouse</h2>
-<p>${ ref('person', relations.spouse) }</p>`;
-        }
+<p>${ref("person", relations.spouse)}</p>`;
+    }
 
-        if (relations.parents != null && relations.parents.length) {
-            s += sqzhtml`
+    if (relations.parents != null && relations.parents.length) {
+      s += sqzhtml`
 <h2>Parents</h2>
 <ul>`;
-            for (const parent_relation of relations.parents) {
-                s += sqzhtml`
-    <li>${ ref('person', parent_relation.person_id) }</li>
+      for (const parent_relation of relations.parents) {
+        s += sqzhtml`
+    <li>${ref("person", parent_relation.person_id)}</li>
   `;
-            }
-            s += sqzhtml`
+      }
+      s += sqzhtml`
 </ul>
 `;
-        }
+    }
 
-        if (relations.children != null && relations.children.length) {
-            s += sqzhtml`
+    if (relations.children != null && relations.children.length) {
+      s += sqzhtml`
 <h2>Children</h2>
 <ul>`;
-            for (const child_relation of relations.children) {
-                s += sqzhtml`
-    <li><p>${ ref('person', child_relation.person_id) }</p></li>
+      for (const child_relation of relations.children) {
+        s += sqzhtml`
+    <li><p>${ref("person", child_relation.person_id)}</p></li>
   `;
-            }
-            s += sqzhtml`
+      }
+      s += sqzhtml`
 </ul>
 `;
-        }
+    }
 
-        let friends_all = [].concat(relations.friends ?? [],
-                                    relations.friend_of ?? [])
+    let friends_all = [].concat(
+      relations.friends ?? [],
+      relations.friend_of ?? []
+    );
 
-        if (friends_all.length) {
-            s += sqzhtml`
+    if (friends_all.length) {
+      s += sqzhtml`
 <h2>Friends</h2>
 <ul>`;
-            for (const friend_relation of friends_all) {
-
-                let friend_detail_text = '';
-                if (friend_relation.friend_detail) {
-                    friend_detail_text = `  (${rdr(friend_relation.friend_detail)})`;
-                }
-
-                s += sqzhtml`
-    <li>${ ref('person', friend_relation.person_id) }${ friend_detail_text }</li>`;
-            }
-            s += sqzhtml`
-</ul>
-`;
+      for (const friend_relation of friends_all) {
+        let friend_detail_text = "";
+        if (friend_relation.friend_detail) {
+          friend_detail_text = `  (${rdr(friend_relation.friend_detail)})`;
         }
 
         s += sqzhtml`
+    <li>${ref("person", friend_relation.person_id)}${friend_detail_text}</li>`;
+      }
+      s += sqzhtml`
+</ul>
+`;
+    }
+
+    s += sqzhtml`
 
 
 <RENDER_ENDNOTES/>
 
-<p class="last-edit">Last modified: ${ data.page.date.toString() }</p>
+<p class="last-edit">Last modified: ${data.page.date.toString()}</p>
 </article>`;
-        
-        return s;
-    }
 
-    return zooflm.make_and_render_document({
-        zoo_flm_environment,
-        render_doc_fn,
-        //doc_metadata: {},
-        render_endnotes: {
-            annotations: ['sectioncontent'],
-        }
-    });
+    return s;
+  };
+
+  return zooflm.make_and_render_document({
+    zoo_flm_environment,
+    render_doc_fn,
+    //doc_metadata: {},
+    render_endnotes: {
+      annotations: ["sectioncontent"],
+    },
+  });
 };
 
-
-module.exports = { data, render, };
+module.exports = { data, render };
